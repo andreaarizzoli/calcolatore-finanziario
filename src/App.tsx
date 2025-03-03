0ed98b27caf7c1495016f3ce800ca10d3d314dd4
andrea arizzoli <andrea.arizzoli.ext@illimity.com>
2025-03-03 12:17:34 +0400

    feat: small fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import "./App.css";
 import Dashboard from "./features/futureCapitalSimulator/Dashboard";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="calcolatore-finanziario" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
       <Dashboard />
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   // <React.StrictMode>
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="calcolatore-finanziario" element={<App />}></Route>
-    </Routes>
-  </BrowserRouter>
+  <App />
   // </React.StrictMode>
 );
 
